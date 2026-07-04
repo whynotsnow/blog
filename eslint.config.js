6b57a893fe1af7a7a98452602cc55ac0760b3c47
whynotsnow <whynotsnow@163.com>
2026-07-04 20:27:27 +0800

    fix: configure astro eslint parser explicitly

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,54 @@
 import js from "@eslint/js";
+import astroParser from "astro-eslint-parser";
 import astro from "eslint-plugin-astro";
 import svelte from "eslint-plugin-svelte";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+
+const projectGlobals = {
+	...globals.browser,
+	...globals.node,
+	...globals.es2024,
+};
+
+const commonRules = {
+	"no-empty": "error",
+	"no-empty-pattern": "error",
+	"no-undef": "off",
+	"no-unused-expressions": "off",
+	"no-useless-escape": "error",
+	"no-var": "error",
+	"prefer-const": "error",
+	"prefer-rest-params": "error",
+	"no-unused-vars": "off",
+};
+
+const typescriptRules = {
+	"@typescript-eslint/ban-ts-comment": "error",
+	"@typescript-eslint/no-empty-object-type": "error",
+	"@typescript-eslint/no-unsafe-function-type": "error",
+	"@typescript-eslint/no-this-alias": "error",
+	"@typescript-eslint/no-unused-expressions": "error",
+	"@typescript-eslint/triple-slash-reference": "off",
+	"@typescript-eslint/no-unused-vars": [
+		"error",
+		{
+			argsIgnorePattern: "^_",
+			varsIgnorePattern: "^_",
+			caughtErrorsIgnorePattern: "^_",
+		},
+	],
+	"@typescript-eslint/no-explicit-any": "off",
+};
+
+const astroRules = {
+	"astro/no-unused-define-vars-in-style": "error",
+};
+
+const svelteRules = {
+	"svelte/no-at-html-tags": "off",
+	"svelte/require-each-key": "error",
+};
 
 export default [
 	{
@@ -14,50 +60,43 @@ export default [
 	...svelte.configs["flat/recommended"],
 	...svelte.configs["flat/prettier"],
 	{
-		files: ["src/**/*.{js,mjs,cjs,ts,astro,svelte}"],
+		files: ["src/**/*.{js,mjs,cjs,ts}"],
 		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-				...globals.es2024,
-			},
+			globals: projectGlobals,
 		},
 		rules: {
-			"no-empty": "error",
-			"no-empty-pattern": "error",
-			"no-undef": "off",
-			"no-unused-expressions": "off",
-			"no-useless-escape": "error",
-			"no-var": "error",
-			"prefer-const": "error",
-			"prefer-rest-params": "error",
-			"astro/no-unused-define-vars-in-style": "error",
-			"@typescript-eslint/ban-ts-comment": "error",
-			"@typescript-eslint/no-empty-object-type": "error",
-			"@typescript-eslint/no-unsafe-function-type": "error",
-			"@typescript-eslint/no-this-alias": "error",
-			"@typescript-eslint/no-unused-expressions": "error",
-			"@typescript-eslint/triple-slash-reference": "off",
-			"svelte/no-at-html-tags": "off",
-			"svelte/require-each-key": "error",
-			"no-unused-vars": "off",
-			"@typescript-eslint/no-unused-vars": [
-				"error",
-				{
-					argsIgnorePattern: "^_",
-					varsIgnorePattern: "^_",
-					caughtErrorsIgnorePattern: "^_",
-				},
-			],
-			"@typescript-eslint/no-explicit-any": "off",
+			...commonRules,
+			...typescriptRules,
 		},
 	},
 	{
 		files: ["src/**/*.svelte"],
 		languageOptions: {
+			globals: projectGlobals,
 			parserOptions: {
 				parser: tseslint.parser,
 			},
+		},
+		rules: {
+			...commonRules,
+			...typescriptRules,
+			...svelteRules,
+		},
+	},
+	{
+		files: ["src/**/*.astro"],
+		languageOptions: {
+			globals: projectGlobals,
+			parser: astroParser,
+			parserOptions: {
+				parser: tseslint.parser,
+				extraFileExtensions: [".astro"],
+			},
+		},
+		rules: {
+			...commonRules,
+			...typescriptRules,
+			...astroRules,
 		},
 	},
 ];
